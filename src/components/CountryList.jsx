5f18f64833986688bff218d2ce0bb545f2ee72db
ev <etersh810@gmail.com>
2024-05-07 21:01:08 -0400

    Add Context API by creating CitiesContext

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,9 +2,12 @@ import Spinner from './Spinner';
 import CountryItem from './CountryItem';
 import Message from './Message';
 import styles from './CountryList.module.css';
+import { useCities } from '../contexts/CitiesContext';
 // import PropTypes from 'prop-types';
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
