4c582cc0d0df38c7254dc3b5587a40283987af83
ev <etersh810@gmail.com>
2024-05-13 21:23:13 -0400

    Add simulated Authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,35 @@
-import { useState } from "react";
-import PageNav from "../components/PageNav";
-import styles from "./Login.module.css";
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/FakeAuthContext';
+import PageNav from '../components/PageNav';
+import styles from './Login.module.css';
+import Button from '../components/Button';
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+  const [email, setEmail] = useState('jack@example.com');
+  const [password, setPassword] = useState('qwerty');
+
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (email && password) login(email, password);
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate('/app', { replace: true });
+    },
+    [isAuthenticated, navigate]
+  );
 
   return (
-    <main className={styles.login}>
+    <main className={styles.login} onSubmit={handleSubmit}>
       <PageNav />
-      
+
       <form className={styles.form}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
@@ -33,7 +52,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
